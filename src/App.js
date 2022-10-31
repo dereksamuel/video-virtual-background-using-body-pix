26f4e42165853a917103ef72ab48bfe0168d52cc
Derek Paul <11dereksamuel@gmail.com>
2022-10-31 11:09:47 -0500

    The blur is functional 👁️

--- src/App.js
+++ src/App.js
@@ -1,24 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable react-hooks/exhaustive-deps */
+import * as tf from "@tensorflow/tfjs";
+import * as bodyPix from "@tensorflow-models/body-pix";
+import Webcam from "react-webcam";
+import { useRef, useState } from 'react';
 
 function App() {
+  const [stopBucle, setStopBucle] = useState(false);
+  const webcamRef = useRef(null);
+  const canvasRef = useRef(null);
+
+  function goBodySegment() {
+    setStopBucle(false);
+
+    bodyPix.load({
+      multiplier: 0.75,
+      stride: 32,
+      quantBytes: 4
+    })
+      .then(net => {
+        segmentationStart(net);
+      })
+      .catch(error => console.error("[error-model]:", error));
+  };
+
+  async function segmentationStart (net) {
+    if (webcamRef.current && webcamRef.current.video.readyState === 4) {
+      const $video = webcamRef.current.video;
+      const videoWidth = $video.videoWidth;
+      const videoHeight = $video.videoHeight;
+
+      // set video props
+      webcamRef.current.video.width = videoWidth;
+      webcamRef.current.video.height = videoHeight;
+
+      // set canvas w and h
+      canvasRef.current.width = videoWidth;
+      canvasRef.current.height = videoHeight;
+ 
+      while(!stopBucle) {
+        // detect
+        const person = await net.segmentPerson($video);
+
+        const backgroundBlurAmount = 6;
+        const edgeBlurAmount = 2;
+        const flipHorizontal = true;
+
+        // draw
+        bodyPix.drawBokehEffect(
+          canvasRef.current,
+          $video,
+          person,
+          backgroundBlurAmount,
+          edgeBlurAmount,
+          flipHorizontal
+        );
+      }
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Webcam ref={webcamRef} className="webcam" />
+      <canvas ref={canvasRef} />
+      <button onClick={goBodySegment}>blur</button>
+      <button onClick={() => setStopBucle(false)}>unblur</button>
+    </>
   );
 }
 
