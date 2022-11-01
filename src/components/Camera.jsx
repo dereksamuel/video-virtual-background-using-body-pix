c19b11bba7af412504b179706a862fc67d53d675
Derek Paul <11dereksamuel@gmail.com>
2022-11-01 09:52:45 -0500

    filter redisign

--- src/components/Camera.jsx
+++ src/components/Camera.jsx
@@ -4,28 +4,31 @@ import * as tf from "@tensorflow/tfjs";
 import * as bodyPix from "@tensorflow-models/body-pix";
 import imageHacker from "../assets/hacker-bg.jpg";
 import Webcam from "react-webcam";
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 function Camera() {
   const [stopBucle, setStopBucle] = useState(false);
+  const [url, setUrl] = useState("");
+  const [loading, setLoading] = useState(true);
+  const [net, setNet] = useState(null);
   const webcamRef = useRef(null);
   const canvasRef = useRef(null);
 
-  function goBodySegment(effect = "blur") {
+  function goBodySegment() {
     setStopBucle(false);
-
     bodyPix.load({
       multiplier: 0.75,
       stride: 32,
       quantBytes: 4
     })
-      .then(net => {
-        segmentationStart(net, effect);
+      .then(localNet => {
+        setNet(localNet);
       })
       .catch(error => console.error("[error-model]:", error));
   };
 
-  async function segmentationStart (net, effect) {
+  async function segmentationStart(effect = "blur") {
+    setLoading(true);
     if (webcamRef.current && webcamRef.current.video.readyState === 4) {
       const $video = webcamRef.current.video;
       const videoWidth = $video.videoWidth;
@@ -40,13 +43,13 @@ function Camera() {
       canvasRef.current.height = videoHeight;
 
       const context = canvasRef.current.getContext("2d");
-      context.clearRect(0, 0, canvasRef.current.width, canvasRef.current.height);
  
       while(!stopBucle) {
         // detect
         const person = await net.segmentPerson($video, {
           internalResolution: "low",
         });
+        setLoading(false);
 
         const backgroundBlurAmount = 6;
         const edgeBlurAmount = 2;
@@ -63,18 +66,14 @@ function Camera() {
             flipHorizontal
           );
         } else {
-          drawMask($video, net, person, context);
+          drawMask($video, person, context);
         }
       }
     }
   };
 
-  async function drawMask($video, net, person, context) {
-    const edgeBlurAmount = 2;
-    const flipHorizontal = true;
-
+  async function drawMask($video, person, context) {
     const mask = bodyPix.toMask(person);
-    const opacity = 1;
 
     // create tempCanvas
     const tempCanvas = document.createElement("canvas");
@@ -83,7 +82,7 @@ function Camera() {
     const tempCtx = tempCanvas.getContext("2d");
 
     tempCtx.putImageData(mask, 0, 0);
-    canvasRef.current.style = `background: url(${imageHacker})`;
+    canvasRef.current.style = `background: url(${url || imageHacker})`;
     context.drawImage($video, 0, 0, canvasRef.current.width, canvasRef.current.height);
     context.save();
     context.globalCompositeOperation = "destination-out";
@@ -91,14 +90,32 @@ function Camera() {
     context.restore();
   }
 
+  const onChangeImage = (event) => {
+    setUrl(URL.createObjectURL(event.target.files[0]));
+  };
+
+  useEffect(() => {
+    goBodySegment();
+  }, []);
+
   return (
     <>
       <Webcam ref={webcamRef} className="webcam" />
       <div className="canvas-container">
+        {loading && <p className="loading">Loading</p>}
         <canvas ref={canvasRef} />
       </div>
-      <button onClick={() => goBodySegment("blur")}>blur</button>
-      <button onClick={() => goBodySegment("image-bg")}>image-bg</button>
+      <div className="filters">
+        <label>
+          <p>blur filter:</p>
+          <button onClick={() => segmentationStart("blur")}>blur</button>
+        </label>
+        <label>
+          <p>image filter:</p>
+          <input type="file" onChange={onChangeImage} />
+          <button onClick={() => segmentationStart("bg-image")} disabled={!url}>Apply filter</button>
+        </label>
+      </div>
       {/* <button onClick={() => setStopBucle(true)}>stop</button> */}
     </>
   );
